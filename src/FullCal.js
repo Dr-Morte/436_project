af007ddb64280fe92ccbb59ccca3532b1369492d
James Vanderburg <james@vanderburg.org>
2019-04-19 16:10:49 -0500

    Add colors

--- src/FullCal.js
+++ src/FullCal.js
@@ -5,6 +5,11 @@ import listPlugin from '@fullcalendar/list'
 // import moment from 'moment'
 import './main.scss'
 
+const capstone = '#0000ff';
+const hci = '#00ff00';
+const ethics = '#ff0000';
+const black = '#000000';
+
 class FullCal extends Component {
 
   // calendarRef = React.createRef()
@@ -27,14 +32,13 @@ class FullCal extends Component {
         }}
         plugins={[ listPlugin, dayGridPlugin ]}
         events={[
-          { title: 'event 1', date: '2019-04-20' },
-          { title: 'event 2', date: '2019-04-22' },
-          { title: 'event 3', date: '2019-04-23' },
-          { title: 'event 4', date: '2019-04-23' },
-          { title: 'event 5', date: '2019-04-23' },
-          { title: 'event 6', date: '2019-04-26' },
-          { title: 'event 7', date: '2019-04-26' },
-
+          { title: 'event 1', date: '2019-04-20', color: hci, textColor: black },
+          { title: 'event 2', date: '2019-04-22', color: capstone },
+          { title: 'event 3', start: '2019-04-23T15:55', color: hci, textColor: black },
+          { title: 'event 4', date: '2019-04-23', color: hci, textColor: black },
+          { title: 'event 5', date: '2019-04-23', color: ethics },
+          { title: 'event 6', date: '2019-04-26', color: hci, textColor: black },
+          { title: 'event 7', date: '2019-04-26', color: capstone }
         ]}
       />
     )
