5c0451e71edfc12a19669f505a6aa1e6188067e4
James Vanderburg <james@vanderburg.org>
2019-04-19 16:18:10 -0500

    Use SCSS instead of CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './main.scss';
 import FullCal from './FullCal';
 import * as serviceWorker from './serviceWorker';
 
