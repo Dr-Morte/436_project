8538fa788f9cc3afc296a3ce75a84a5cafd5f784
James Vanderburg <james@vanderburg.org>
2019-04-19 00:08:28 -0500

    Set up flexbox container

--- src/FullCal.js
+++ src/FullCal.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import listPlugin from '@fullcalendar/list'
-import moment from 'moment'
+// import moment from 'moment'
 import './main.scss'
 
 class FullCal extends Component {
 
+  // calendarRef = React.createRef()
+  //
+  // componentDidMount(){
+  //   console.log(this.calendarRef);
+  //   let calendarApi = this.calendarRef.current.getApi()
+  //   calendarApi.updateSize()
+  // }
+
   render() {
     return (
       <FullCalendar
+        ref={this.calendarRef}
         defaultView="listYear"
         header={{
           left: 'prev,next today',
