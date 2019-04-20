81891418d436c1f9f2b538d2f9c5b0dbe29378a5
James Vanderburg <james@vanderburg.org>
2019-04-19 23:01:25 -0500

    Add Primer and notification badges

--- src/ClassBox.js
+++ src/ClassBox.js
@@ -3,17 +3,20 @@ import './main.scss';
 
 class ClassBox extends Component {
   render() {
+
     var styles = {
       backgroundColor: this.props.color
     };
 
     return (
-      <div>
-        <div class="classheader">
-          <span class="dot" style={styles}></span>
+      <div className="hover-grow">
+        <div className="classheader">
+          <span className="dot" style={styles}></span>
           <a href="#">{this.props.code}</a>
+          <span title="Notifications" id="badge" className="State State--red">{this.props.events}</span>
+
         </div>
-        <span class="coursename">{this.props.name}</span>
+        <span className="coursename">{this.props.name}</span>
       </div>
     );
   }
--- src/ClassList.js
+++ src/ClassList.js
@@ -10,30 +10,35 @@ class ClassList extends Component {
           <ClassBox
             code="CSCE 420"
             name="Artificial Intelligence"
+            events="2"
             color="#0366d6"/>
         </li>
         <li>
           <ClassBox
             code="CSCE 436"
             name="Human Computer Interaction"
+            events="1"
             color="#28a745"/>
         </li>
         <li>
           <ClassBox
             code="CSCE 482"
             name="Senior Capstone Design"
+            events="1"
             color="#6f42c1"/>
         </li>
         <li>
           <ClassBox
             code="MATH 140"
             name="Mathematics for Business and Social Sciences"
+            events="2"
             color="#ffd33d"/>
         </li>
         <li>
           <ClassBox
             code="MATH 308"
             name="Differential Equations"
+            events="1"
             color="#d73a49"/>
         </li>
       </ul>
